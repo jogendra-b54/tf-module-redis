ef7ba08141967409643ca798bd5a5431b7ddf552
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-18 13:06:43 +0530

    Auto push

--- sg.tf
+++ sg.tf
@@ -1,32 +1,32 @@
-# resource "aws_security_group" "allows_docdb" {
-#   name        = "roboshop allows_docdb_internal only"
-#   description = "allows_docdb_internal only"
-#   vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
+resource "aws_security_group" "allows_redis" {
+  name        = "roboshop allows_redis_internal only"
+  description = "allows_redis_internal only"
+  vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
 
-#   ingress {
-#     description = "docdb from VPC"
-#     from_port   = 27017
-#     to_port     = 27017
-#     protocol    = "tcp"
-#     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]     
-#   } 
+  ingress {
+    description = "redis from VPC"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]     
+  } 
 
-#    ingress {
-#     description = "docdb from Default VPC"
-#     from_port   = 27017
-#     to_port     = 27017
-#     protocol    = "tcp"
-#     cidr_blocks = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]     
-#   } 
+   ingress {
+    description = "redis from Default VPC"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]     
+  } 
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   } 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
 
-#   tags = {
-#     Name = "roboshop-${var.ENV}-docdb-sg"
-#   }
-# }
+  tags = {
+    Name = "roboshop-${var.ENV}-redis-sg"
+  }
+}
