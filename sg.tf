9f9650dbff5f3f05ecafaa76f52b9b2e5fbd1c88
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-10 11:48:38 +0530

    Auto push

--- sg.tf
+++ sg.tf
@@ -1,3 +1,40 @@
+# resource "aws_security_group" "allows_redis" {
+#   name        = "roboshop allows_redis_internal only"
+#   description = "allows_redis_internal only"
+#   vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+#   ingress {
+#     description = "redis from VPC"
+#     from_port   = var.REDIS_PORT_NUMBER
+#     to_port     = var.REDIS_PORT_NUMBER
+#     protocol    = "tcp"
+#     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]     
+#   } 
+
+#    ingress {
+#     description = "redis from Default VPC"
+#     from_port   = var.REDIS_PORT_NUMBER
+#     to_port     = var.REDIS_PORT_NUMBER
+#     protocol    = "tcp"
+#     cidr_blocks = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]     
+#   } 
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   } 
+
+#   tags = {
+#     Name = "roboshop-${var.ENV}-redis-sg"
+#   }
+# }
+
+
+#================================DELETE BELOW LINES======================
+
+
 resource "aws_security_group" "allows_redis" {
   name        = "roboshop allows_redis_internal only"
   description = "allows_redis_internal only"
@@ -5,16 +42,16 @@ resource "aws_security_group" "allows_redis" {
 
   ingress {
     description = "redis from VPC"
-    from_port   = var.REDIS_PORT_NUMBER
-    to_port     = var.REDIS_PORT_NUMBER
+    from_port   = 6379
+    to_port     = 6379
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]     
   } 
 
    ingress {
     description = "redis from Default VPC"
-    from_port   = var.REDIS_PORT_NUMBER
-    to_port     = var.REDIS_PORT_NUMBER
+    from_port   = 6379
+    to_port     = 6379
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]     
   } 
--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "ENV" {}
-variable "REDIS_PORT_NUMBER" {}
-variable "REDIS_INSTANCE_TYPE" {}
-variable "REDIS_INSTANCE_COUNT" {}
-variable "REDIS_ENGINE_VERSION" {}
+#variable "REDIS_PORT_NUMBER" {}
+#variable "REDIS_INSTANCE_TYPE" {}
+#variable "REDIS_INSTANCE_COUNT" {}
+#variable "REDIS_ENGINE_VERSION" {}
      
