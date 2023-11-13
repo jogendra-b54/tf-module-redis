8646b8ca7bca3e50b28a95fc53b021b46b6a4cd3
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-13 17:23:31 +0530

    Auto push

--- route53.tf
+++ route53.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "redis_dns_record" {
   name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
   type    = "CNAME"
   ttl     = 10
-  records = [aws_elasticache_cluster.redis.cache_nodes[0]]
+  records = [aws_elasticache_cluster.redis.cache_nodes[0].address]
 }
 
 
